954d0885a1df564b739672c930615a50a5bf7cda
Giacomo Bergami <bergamigiacomo@gmail.com>
2021-10-24 21:54:00 +0100

    Update RigidbodyFirstPersonController.cs

--- Assets/Standard Assets/Characters/FirstPersonCharacter/Scripts/RigidbodyFirstPersonController.cs
+++ Assets/Standard Assets/Characters/FirstPersonCharacter/Scripts/RigidbodyFirstPersonController.cs
@@ -16,26 +16,30 @@ namespace UnityStandardAssets.Characters.FirstPerson
     {
         // Moves the camera by just using the TCP messages
         private static bool useTcp = true;
+	
         // Using the input keyboard/mouse method
         private static bool useInput = false;
 
         [Serializable]
         public class MovementSettings
         {
-            public float ForwardSpeed = 8.0f;   // Speed when walking forward
-            public float BackwardSpeed = 4.0f;  // Speed when walking backwards
-            public float StrafeSpeed = 4.0f;    // Speed when walking sideways
-            public float RunMultiplier = 2.0f;   // Speed when sprinting
-            public KeyCode RunKey = KeyCode.LeftControl;
-            public float JumpForce = 30f;
+            public float ForwardSpeed = 8.0f;              // Speed when walking forward
+            public float BackwardSpeed = 4.0f;             // Speed when walking backwards
+            public float StrafeSpeed = 4.0f;               // Speed when walking sideways
+            public float RunMultiplier = 2.0f;             // Speed when sprinting
+            public KeyCode RunKey = KeyCode.LeftControl;   // Key for running (increase the spead)
+            public float JumpForce = 30f;                  // Force associated to the jump
             public AnimationCurve SlopeCurveModifier = new AnimationCurve(new Keyframe(-90.0f, 1.0f), new Keyframe(0.0f, 1.0f), new Keyframe(90.0f, 0.0f));
             [HideInInspector] public float CurrentTargetSpeed = 8f;
-            public MouseLook mouseLook = new MouseLook();
+            public MouseLook mouseLook = new MouseLook();  // Mouse orientation for rotation
 
 #if !MOBILE_INPUT
             private bool m_Running;
 #endif
 
+            /*
+	     * Some initialization, that is mainly performed only if there is the basic UNITY setting, with no TCP server
+	     */
             public void Init(Transform transform, Transform cam)
             {
                 mouseLook.Init(transform, cam);
@@ -46,8 +50,13 @@ namespace UnityStandardAssets.Characters.FirstPerson
             public bool hitJump = false;
             public bool hitRun = false;
 
+       
             public void Update()
-            {
+            { 
+	        /*
+		 * Updating the Rover's setting via the keyboard/mouse if and only if 
+		 * I enabled the reading from the input
+		 */
                 if (useInput)
                 {
                     hitJump = CrossPlatformInputManager.GetButtonDown("Jump");
@@ -59,17 +68,17 @@ namespace UnityStandardAssets.Characters.FirstPerson
 
             public void FixedUpdate()
             {
-if (useInput)
+	        // Same as above
+                if (useInput)
                 {
                     x = CrossPlatformInputManager.GetAxis("Horizontal");
                     y = CrossPlatformInputManager.GetAxis("Vertical");
                 }
-                //Debug.Log(x);
             }
 
             public Vector2 GetInput()
             {
-
+                // Reading the values, either from the keyboard, or from the TCP packet
                 Vector2 input = new Vector2
                 (
                     x, y
@@ -118,7 +127,7 @@ if (useInput)
 
             internal void LookRotation(Transform transform1, Transform transform2)
             {
-                Debug.Log(xRot);
+                ///Debug.Log(xRot);
                 mouseLook.LookRotation(transform1, transform2, xRot, yRot);
             }
 #endif
@@ -136,22 +145,34 @@ if (useInput)
             public float shellOffset; //reduce the radius by that ratio to avoid getting stuck in wall (a value of 0.1f is nice)
         }
 
+        /*
+	 * TCP communication utility class.
+	 * I don't know which are the specs for the actual Rover, but I suppose that it would be
+	 * better to have a UDP communication, as packet loss is not a major problem (take only the most recent one,
+	 * and discard the others, by using Lamport's clocks).
+	 */ 
         public class TCP
         {
+	    // Buffer from which read the packages. Ideally, this should be the
+	    // maximum size of a TCP packet, which is 65535. Ideally, we need to
+	    // receive less information than that!
             public static int dataBufferSize = 4096;
+	    
+	    // IP associated to the ML server
             public string ip = "127.0.0.1";
+	    // Port associated to the ML server
             public int port = 60260;
+	    // Aforementioned Rover state update, where the movement information
+	    // is going to be modified by the received TCP message
             MovementSettings settingsToMove;
 
-
+            // Setting the Rover's information class
             public TCP(MovementSettings ms)
             {
                 settingsToMove = ms;
             }
 
-            ///Packet receivedData;
             public TcpClient socket;
-
             private NetworkStream stream;
             private byte[] receiveBuffer;
 
@@ -162,42 +183,44 @@ if (useInput)
                     ReceiveBufferSize = dataBufferSize,
                     SendBufferSize = dataBufferSize
                 };
-
                 receiveBuffer = new byte[dataBufferSize];
+		
+		// Establishing the initial handshake with the server
                 socket.BeginConnect(ip, port, ConnectCallback, socket);
             }
 
             private void ConnectCallback(IAsyncResult _result)
             {
+	        // On finish, everything is OK, closing the handshake procedure
                 socket.EndConnect(_result);
-
-                if (!socket.Connected)
-                {
+                if (!socket.Connected) {
                     return;
                 }
-
+		// Establishing the stream (C# abstraction) from the socket (usual C representation)
                 stream = socket.GetStream();
-
-                ///receivedData = new Packet();
-
-                
             }
+	    
+	    /*
+	     * In this application, whenever we send the screen data, we expect to receive
+	     * in return the actions that are required to atonomously move the Rover
+	     */
             public void SendData(byte[] _packet)
             {
-                try
-                {
-                    if (socket != null)
-                    {
+                try {
+                    if (socket != null) {
+		        // Sending the screenshot in jpg format
                         stream.BeginWrite(_packet, 0, _packet.Length, null, null);
+			// Expecting to receive some data from the server
                         stream.BeginRead(receiveBuffer, 0, dataBufferSize, ReceiveCallback, null);
                     }
-                }
-                catch (Exception _ex)
-                {
+                } catch (Exception _ex) {
                     Debug.Log($"Error sending data to server via TCP: {_ex}");
                 }
             }
 
+            /*
+	     * Asynchronous method that is invoked when we receive a result form the server
+	     */ 
             private void ReceiveCallback(IAsyncResult _result)
             {
                 Debug.Log("ReceiveCallback");
@@ -214,7 +237,6 @@ if (useInput)
 
                     byte[] _data = new byte[_byteLength];
                     Array.Copy(receiveBuffer, _data, _byteLength);
-
                     HandleData(_data);
                     ///stream.BeginRead(receiveBuffer, 0, dataBufferSize, ReceiveCallback, null);
                 }
@@ -226,14 +248,19 @@ if (useInput)
                 }
             }
 
+            /*
+	     * Returning the number of occurrences of character c in S
+	     */
             private static float getNOccurences(string S, char c)
             {
                 return S.Split(c).Length - 1;
             }
 
-            private bool HandleData(byte[] _data)
-            {
-
+            /*
+	     * Receiving the data from the server
+	     */
+            private bool HandleData(byte[] _data) {
+                // Representing the received data as a single string
                 var str = System.Text.Encoding.Default.GetString(_data);
                 Debug.Log(str);
                 int _packetLength = 0;
@@ -244,6 +271,7 @@ if (useInput)
                 settingsToMove.xRot = 0;
                 settingsToMove.yRot = 0;
 
+                // More efficient implementation: iteration over str[i] for each i
                 if (str.Length > 0)
                 {
                     // Jump
@@ -273,9 +301,8 @@ if (useInput)
                 return false;
             }
         }
-
-    
-
+	
+        // Client-Server communication
         public TCP tcp;
 
         public Camera cam;
@@ -291,7 +318,6 @@ if (useInput)
         public static int resHeight = 3300;
 
 
-        // Continuously sending data to the server
         private void LateUpdate() {
             if (tcp != null) {
                 RenderTexture rt = new RenderTexture(resWidth, resHeight, 24);
@@ -303,6 +329,7 @@ if (useInput)
                 cam.targetTexture = null;
                 RenderTexture.active = null; // JC: added to avoid errors
                 Destroy(rt);
+		// Sending the TCP data to the server, expecting some result from it
                 tcp.SendData(screenShot.EncodeToJPG());
             }
 
@@ -315,11 +342,13 @@ if (useInput)
             m_Capsule = GetComponent<CapsuleCollider>();
             if (useTcp)
             {
+	        // Setting the connection to the Server iff. this is up
                 tcp = new TCP(movementSettings);
                 tcp.Connect();
             }
             if (useInput)
             {
+	        // Using the keystroke and input only if required
                 movementSettings.Init(transform, cam.transform);
             }
         }
@@ -385,7 +414,7 @@ if (useInput)
 
                 if (m_Jump)
                 {
-                    Debug.Log("does jump");
+                    ///Debug.Log("does jump");
                     m_RigidBody.drag = 0f;
                     m_RigidBody.velocity = new Vector3(m_RigidBody.velocity.x, 0f, m_RigidBody.velocity.z);
                     m_RigidBody.AddForce(new Vector3(0f, movementSettings.JumpForce, 0f), ForceMode.Impulse);
@@ -405,22 +434,6 @@ if (useInput)
             float angle = Vector3.Angle(m_GroundContactNormal, Vector3.up);
             return movementSettings.SlopeCurveModifier.Evaluate(angle);
         }
-
-
-
-
-        /*private Vector2 GetInput()
-        {
-
-            Vector2 input = new Vector2
-            {
-                x = CrossPlatformInputManager.GetAxis("Horizontal"),
-                y = CrossPlatformInputManager.GetAxis("Vertical")
-            };
-            movementSettings.UpdateDesiredTargetSpeed(input);
-            return input;
-        }*/
-
 
         private void RotateView()
         {
