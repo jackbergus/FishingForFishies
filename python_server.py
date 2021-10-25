dbd08a7c7ff5add2f6b365a27338d1233dd6b119
Giacomo Bergami <bergamigiacomo@gmail.com>
2021-10-25 09:25:25 +0100

    Added comments to the server

--- python_server.py
+++ python_server.py
@@ -8,27 +8,28 @@ from datetime import datetime
 
 def recvall(sock):
     BUFF_SIZE = 4096 # 4 KiB
-    data = b''
+    data = b''       # binary data (not utf-8 nor ascii)
     while True:
         part = sock.recv(BUFF_SIZE)
-        data += part
+        data += part # append the data
         if len(part) < BUFF_SIZE:
             # either 0 or end of data
             break
     return data
 
 def write_image_to_file_incrementally(image):
+    """
+    Dumping the image to a continuously progressing file, just for debugging puroses
+    """
     i = 0
     while os.path.exists("sample%s.jpeg" % i):
         i += 1
-
     with open("sample%s.jpeg" % i, "wb") as f:
         f.write(image)
 
 def relational_learning_model(image):
     # If you want, you can also dump the image into a file, just for testing that it works, forsooth!
     write_image_to_file_incrementally(image)
-    
     # returns the actions to be performed by the rover
     # See the unity source code for some specs
     return "F"
@@ -58,10 +59,11 @@ def server(host='127.0.0.1', port=60260):
 
         # the image is completely received
         print("[+] Received", len(binary_image))
-        # TODO: do something with the image
-
+        # TODO: do something with the image. E.g., send it to the
+        # relational learning model and return the set of actions
+        # that the simulated rover has to perform
         actions = relational_learning_model(binary_image)
-        
+        # Encode the string via utf-8, and send the result as a byte array
         c.sendall(actions.encode('utf-8'))
         print("[+] Sending to {0}:{1}".format(addr[0], addr[1]))
 
